24a6af98b98ea97e8643490bbc580a2e2ad7e460
yunze <yunze.moe@outlook.com>
2019-07-25 19:48:18 +0800

    [Fixed] Error occurs when no update found.

--- cmd/update.go
+++ cmd/update.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2019 NAME HERE <EMAIL ADDRESS>
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -28,7 +13,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// updateCmd represents the update command
 var updateCmd = &cobra.Command{
 	Use:   "update",
 	Short: "Update your subscriptions and download.",
@@ -43,14 +27,13 @@ func init() {
 }
 
 func update() {
-
-	// count := 0
 	dataSource := viper.GetString("data-source")
 	if parser.ParserCtor[dataSource] == nil {
 		fmt.Println("Not valid data source: ", dataSource)
 		return
 	}
 
+	count := 0
 	web := parser.ParserCtor[dataSource]()
 
 	for idx, subItem := range globalData.Sublist {
@@ -61,15 +44,17 @@ func update() {
 			"title": 1,
 		}
 		infos := web.ShowFindResult(filterMap, 2)
+
+		newEpNumMap := make(map[float64]bool)
 		selectNums := []int{}
 		for _, info := range infos {
 			title := info[len(info)-1]
 			ep := parseEpisodeFromTitle(title)
-			if ep > subItem.Progress {
+			if ep > subItem.Progress && !newEpNumMap[ep] {
+				newEpNumMap[ep] = true
 				if ep > maxEp {
 					maxEp = ep
 				}
-				// fmt.Println("FIND!!!", info)
 				idx, err := strconv.Atoi(info[0])
 				if err == nil {
 					selectNums = append(selectNums, idx)
@@ -77,48 +62,54 @@ func update() {
 			}
 		}
 
-		magnets := web.GetMagnets(selectNums)
-		needTrimMagnet := viper.GetBool("enable-trim-magnet")
-		if needTrimMagnet {
-			trimMagnets(magnets)
-		}
+		if len(selectNums) > 0 {
+			fmt.Println("Subcription updated:", subItem.Name, "updated to", subItem.Progress, "episode.")
 
-		useNameAsFolder := viper.GetBool("use-name-as-subscription-folder")
-		usePatternAsFolder := viper.GetBool("use-pattern-as-subscription-folder")
-		dir := viper.GetString("default-download-dir")
-
-		if _, err := os.Stat(dir); os.IsNotExist(err) {
-			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Println("Cannot create Download folder:", err)
-				return
-			}
-		}
-
-		if useNameAsFolder || usePatternAsFolder {
-			var dirTest string
-			if useNameAsFolder && subItem.Name != "" {
-				dirTest = path.Join(dir, subItem.Name)
-			} else {
-				dirTest = path.Join(dir, subItem.Pattern)
+			magnets := web.GetMagnets(selectNums)
+			needTrimMagnet := viper.GetBool("enable-trim-magnet")
+			if needTrimMagnet {
+				trimMagnets(magnets)
 			}
 
-			if _, err := os.Stat(dirTest); os.IsNotExist(err) {
-				if err := os.Mkdir(dirTest, 0755); err == nil {
-					dir = dirTest
+			useNameAsFolder := viper.GetBool("use-name-as-subscription-folder")
+			usePatternAsFolder := viper.GetBool("use-pattern-as-subscription-folder")
+			dir := viper.GetString("default-download-dir")
+
+			if _, err := os.Stat(dir); os.IsNotExist(err) {
+				if err := os.MkdirAll(dir, 0755); err != nil {
+					fmt.Println("Cannot create Download folder:", err)
+					return
 				}
 			}
-		}
 
-		err := downloadMagnets(magnets, dir)
-		if err != nil {
-			fmt.Println("Cannot connect to aria2:", err)
-			return
-		}
+			if useNameAsFolder || usePatternAsFolder {
+				var dirTest string
+				if useNameAsFolder && subItem.Name != "" {
+					dirTest = path.Join(dir, subItem.Name)
+				} else {
+					dirTest = path.Join(dir, subItem.Pattern)
+				}
 
-		subItem.Progress = maxEp
-		subItem.Time = time.Now().Unix()
-		globalData.Sublist[idx] = subItem
+				if _, err := os.Stat(dirTest); os.IsNotExist(err) {
+					if err := os.Mkdir(dirTest, 0755); err == nil {
+						dir = dirTest
+					}
+				}
+			}
+
+			err := downloadMagnets(magnets, dir)
+			if err != nil {
+				fmt.Println("Cannot connect to aria2:", err)
+				return
+			}
+
+			subItem.Progress = maxEp
+			subItem.Time = time.Now().Unix()
+			globalData.Sublist[idx] = subItem
+			count++
+		}
 	}
 
+	fmt.Println("Update completed:", count, "subscription(s) updated.")
 	writeData()
 }
