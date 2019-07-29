ff302d43f717922d453d51f9e7585464f7e4a10c
yunze <yunze.moe@outlook.com>
2019-07-29 21:07:26 +0800

    Use goroutine to implement `bgmgo update`

--- cmd/update.go
+++ cmd/update.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"sync"
 	"time"
 
 	"github.com/myzWILLmake/bgmgo/parser"
@@ -28,103 +29,118 @@ func init() {
 	rootCmd.AddCommand(updateCmd)
 }
 
-func notifyUpdate(updatedSubItems []*SubItem) {
+func notifyUpdate(updatedSubItems chan *SubItem) {
 	title := fmt.Sprintf("Bgmgo Updated %d Subcription(s)", len(updatedSubItems))
 	msg := ""
-	for _, subItem := range updatedSubItems {
-		if subItem.Progress == math.Floor(subItem.Progress) {
-			msg += fmt.Sprintf("%s updated to EP.%d.\n", subItem.Name, int(subItem.Progress))
+	for {
+		if subItem, ok := <-updatedSubItems; ok {
+			if subItem.Progress == math.Floor(subItem.Progress) {
+				msg += fmt.Sprintf("%s updated to EP.%d.\n", subItem.Name, int(subItem.Progress))
+			} else {
+				msg += fmt.Sprintf("%s updated to EP.%.1f.\n", subItem.Name, subItem.Progress)
+			}
 		} else {
-			msg += fmt.Sprintf("%s updated to EP.%.1f.\n", subItem.Name, subItem.Progress)
+			break
 		}
 	}
 	beeep.Notify(title, msg, "")
 }
 
-func update() {
-	updatedSubItems := []*SubItem{}
+func doUpdateSubItem(subItem *SubItem, wg *sync.WaitGroup, updatedSubItems chan *SubItem) {
+	defer wg.Done()
 
-	// TODO: Use goroutine to request
-	for _, subItem := range globalData.Sublist {
-		source := subItem.Source
-		if parser.ParserCtor[source] == nil {
-			fmt.Println("Not valid data source: ", source)
-			continue
-		}
-		web := parser.ParserCtor[source]()
+	source := subItem.Source
+	if parser.ParserCtor[source] == nil {
+		fmt.Println("Not valid data source: ", source)
+		return
+	}
+	web := parser.ParserCtor[source]()
 
-		maxEp := subItem.Progress
-		err := web.Request([]string{subItem.Pattern})
-		if err != nil {
-			fmt.Println("Error: failed to request", source)
-			fmt.Println(err)
-			continue
-		}
-		filterMap := map[string]int{
-			"no":    0,
-			"title": 1,
-		}
-		infos := web.ShowFindResult(filterMap, 2)
+	maxEp := subItem.Progress
+	err := web.Request([]string{subItem.Pattern})
+	if err != nil {
+		fmt.Println("Error: failed to request", source)
+		fmt.Println(err)
+		return
+	}
+	filterMap := map[string]int{
+		"no":    0,
+		"title": 1,
+	}
+	infos := web.ShowFindResult(filterMap, 2)
 
-		newEpNumMap := make(map[float64]bool)
-		selectNums := []int{}
-		for _, info := range infos {
-			title := info[len(info)-1]
-			ep := parseEpisodeFromTitle(title)
-			if ep > subItem.Progress && !newEpNumMap[ep] {
-				newEpNumMap[ep] = true
-				if ep > maxEp {
-					maxEp = ep
-				}
-				idx, err := strconv.Atoi(info[0])
-				if err == nil {
-					selectNums = append(selectNums, idx)
-				}
+	newEpNumMap := make(map[float64]bool)
+	selectNums := []int{}
+	for _, info := range infos {
+		title := info[len(info)-1]
+		ep := parseEpisodeFromTitle(title)
+		if ep > subItem.Progress && !newEpNumMap[ep] {
+			newEpNumMap[ep] = true
+			if ep > maxEp {
+				maxEp = ep
 			}
-		}
-
-		if len(selectNums) > 0 {
-			fmt.Println("Subcription updated:", subItem.Name, "updated to EP.", maxEp)
-
-			magnets := web.GetMagnets(selectNums)
-			needTrimMagnet := viper.GetBool("enable-trim-magnet")
-			if needTrimMagnet {
-				trimMagnets(magnets)
+			idx, err := strconv.Atoi(info[0])
+			if err == nil {
+				selectNums = append(selectNums, idx)
 			}
-
-			useNameAsFolder := viper.GetBool("use-name-as-subscription-folder")
-			dir := viper.GetString("default-download-dir")
-
-			if _, err := os.Stat(dir); os.IsNotExist(err) {
-				if err := os.MkdirAll(dir, 0755); err != nil {
-					fmt.Println("Cannot create Download folder:", err)
-					continue
-				}
-			}
-
-			if useNameAsFolder && subItem.Name != "" {
-				dirTest := path.Join(dir, subItem.Name)
-
-				if _, err := os.Stat(dirTest); os.IsNotExist(err) {
-					if err := os.Mkdir(dirTest, 0755); err == nil {
-						dir = dirTest
-					}
-				} else {
-					dir = dirTest
-				}
-			}
-
-			err := downloadMagnets(magnets, dir)
-			if err != nil {
-				fmt.Println("Cannot connect to aria2:", err)
-				continue
-			}
-
-			subItem.Progress = maxEp
-			subItem.Time = time.Now().Unix()
-			updatedSubItems = append(updatedSubItems, subItem)
 		}
 	}
+
+	if len(selectNums) > 0 {
+		fmt.Println("Subcription updated:", subItem.Name, "updated to EP.", maxEp)
+
+		magnets := web.GetMagnets(selectNums)
+		needTrimMagnet := viper.GetBool("enable-trim-magnet")
+		if needTrimMagnet {
+			trimMagnets(magnets)
+		}
+
+		useNameAsFolder := viper.GetBool("use-name-as-subscription-folder")
+		dir := viper.GetString("default-download-dir")
+
+		if _, err := os.Stat(dir); os.IsNotExist(err) {
+			if err := os.MkdirAll(dir, 0755); err != nil {
+				fmt.Println("Cannot create Download folder:", err)
+				return
+			}
+		}
+
+		if useNameAsFolder && subItem.Name != "" {
+			dirTest := path.Join(dir, subItem.Name)
+
+			if _, err := os.Stat(dirTest); os.IsNotExist(err) {
+				if err := os.Mkdir(dirTest, 0755); err == nil {
+					dir = dirTest
+				}
+			} else {
+				dir = dirTest
+			}
+		}
+
+		err := downloadMagnets(magnets, dir)
+		if err != nil {
+			fmt.Println("Cannot connect to aria2:", err)
+			return
+		}
+
+		subItem.Progress = maxEp
+		subItem.Time = time.Now().Unix()
+		updatedSubItems <- subItem
+	}
+}
+
+func update() {
+	var wg sync.WaitGroup
+	wg.Add(len(globalData.Sublist))
+	updatedSubItems := make(chan *SubItem, len(globalData.Sublist))
+
+	for _, subItem := range globalData.Sublist {
+		go doUpdateSubItem(subItem, &wg, updatedSubItems)
+	}
+
+	fmt.Println("Checking update...")
+	wg.Wait()
+	close(updatedSubItems)
 
 	fmt.Println("Update completed:", len(updatedSubItems), "subscription(s) updated.")
 	if len(updatedSubItems) > 0 {
